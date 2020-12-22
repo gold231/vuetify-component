f8e5c28836b8051491c615be7f0b673d4ba5cc97
Gold813 <goldendragon0318@gmail.com>
2020-12-22 17:37:57 -0500

    vuetify component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-main>
+      <ButtonComp :styles="{
+        state: state[1],
+        fill: fill_color[1],
+        outline: outline_color[0],
+        text: '1,000,000',
+        text_size: 24,
+        select: select[0],
+        select_size: 16,
+        oval: 300
+      }" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ButtonComp from './components/ButtonComp';
+
+export default {
+  name: 'App',
+
+  components: {
+    ButtonComp,
+  },
+
+  data: () => ({
+    state: [ "view", "edit" ],
+    fill_color: [ "blue", "white", "red" ],
+    outline_color: [ "default", "aligned", "misaligned" ],
+    select: [ "per occurrence", "per claim", "per year" ]
+  }),
+};
+</script>
---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="comp" :style="div_style">
+    <p :style="text_style">${{ styles.text }}</p>
+    <p :style="select_style">{{ styles.select }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComp',
+  props: {
+      styles: Object
+  },
+  computed: {
+    // button field's styles
+    div_style () {
+      var divStr = ''; // button field's styles variable
+      
+      divStr += 'width:' + this.styles.oval + 'px;'; // oval size style
+
+      // discriminate that state is "view" or "edit"
+      if (this.styles.state == "view") {
+        divStr += 'border: 2px solid blue;'
+      } else {
+        //outline color style
+        if (this.styles.outline == "aligned") {
+          divStr += 'border: 2px solid blue;'
+        } else if (this.styles.outline == "misaligned") {
+          divStr += 'border: 2px solid red;'
+        } else {
+          divStr += 'border: 2px solid black;';
+        }
+      }
+
+      // if fill color is white, text color is black or text color is white  
+      if (this.styles.fill == "white") {
+        divStr += 'color: black;'
+      } else {
+        divStr += 'color: white;'
+      }
+
+      divStr += 'background-color:' + this.styles.fill;
+
+      return divStr;
+    },
+
+
+    // text field's font size style
+    text_style() {
+      return 'font-size:' + this.styles.text_size + 'px;'
+    },
+
+    //select field's font size style
+    select_style() {
+      return 'font-size:' + this.styles.select_style + 'px;'
+    }    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+//component's general styles
+.comp {   
+  text-align: center;
+  border-radius: 35px;
+  padding: 5px;
+  margin: 5px;
+  p {
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+}
+</style>
